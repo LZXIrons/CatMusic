3e8f6ce29fcaaebb88046d13313d6802fac98643
a8745324@163.com <you@example.com>
2019-05-08 01:22:49 +0800

    508

--- src/api/http.js
+++ src/api/http.js
@@ -43,16 +43,13 @@ const errorHandle = (status, other) => {
  * 请求拦截器
  * 每次请求前，如果存在token则在请求头中携带token
  */
-// API.interceptors.request.use(
-//     res => {
-//         // 登录流程控制中，根据本地是否存在token判断用户的登录情况
-//         // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token
-//         // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码
-//         // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。
+API.interceptors.request.use(
 
-//     },
-//     error => Promise.error(error)
-// )
+    res => Promise.resolve(res) ,
+       
+  
+    error => Promise.error(error)
+)
 
 // 响应拦截器
 API.interceptors.response.use(
